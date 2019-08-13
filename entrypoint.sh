4b6746f0856346803ee910f20acfb1f42a36c2c4
qianji <will6618@qq.com>
2019-08-13 12:57:44 +0800

    Update entrypoint.sh
    更新

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,13 @@
+if [ ! -f "/etc/ssh/ssh_host_rsa_key" ];then
+    ssh-keygen -t rsa -N '' -q -f /etc/ssh/ssh_host_rsa_key
+fi
+/usr/sbin/sshd
+crond
+
 for file in `ls /etc/init.d`
 do if [ -x /etc/init.d/${file} ];  then 
     /etc/init.d/$file restart
 fi done
-crond
 bt default
+
 tail -f /dev/null
